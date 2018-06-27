88f6bb6ecd59729b6af34fd04f3e3ed00cc89c90
Victor <victor.lopez@uniat.com>
2018-06-27 13:48:16 -0500

    socket io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'../public');
-const app = express();
-
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server); // gives access to socket.io.js
 // --------------------------------------------------
-
 app.use(express.static(publicPath));
+// --------------------------------------------------
+io.on('connection', (socket) => {
+    console.log('User connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+// --------------------------------------------------
+server.listen(port, () => {
     console.log('Server started on port ' + port);
 });
